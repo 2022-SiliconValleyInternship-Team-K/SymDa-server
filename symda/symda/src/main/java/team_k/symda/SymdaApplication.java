8c9fedd4b822f3c678678f342270c618db797423
cje1903 <cje1903@naver.com>
2022-07-01 16:22:49 +0900

    init

---
+++ symda/symda/src/main/java/team_k/symda/SymdaApplication.java
@@ -0,0 +1,13 @@
+package team_k.symda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymdaApplication.class, args);
+	}
+
+}
---
+++ symda/symda/src/test/java/team_k/symda/SymdaApplicationTests.java
@@ -0,0 +1,13 @@
+package team_k.symda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
