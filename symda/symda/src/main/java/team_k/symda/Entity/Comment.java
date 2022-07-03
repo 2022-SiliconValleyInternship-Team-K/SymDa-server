36b12872bcbc6aec9b5ee353e177877f822b6993
cje1903 <cje1396@gmail.com>
2022-07-04 03:40:02 +0900

    [FEAT] add Comment domain

---
+++ symda/symda/src/main/java/team_k/symda/Entity/Comment.java
@@ -0,0 +1,26 @@
+package team_k.symda.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter @Setter @ToString
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long question_id;
+
+    private String question;
+
+    public Comment(Long question_id, String question) {
+        this.question_id = question_id;
+        this.question = question;
+    }
+}
