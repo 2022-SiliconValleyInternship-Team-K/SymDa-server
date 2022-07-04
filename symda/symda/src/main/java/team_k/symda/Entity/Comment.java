d95cc95ba4f5fd944f3487d6ba0244eefae05e6d
cje1903 <cje1396@gmail.com>
2022-07-04 14:30:48 +0900

    [CHORE] change ERD
    change comment domain field

--- symda/symda/src/main/java/team_k/symda/Entity/Comment.java
+++ symda/symda/src/main/java/team_k/symda/Entity/Comment.java
@@ -4,10 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter @Setter @ToString
 @Entity
@@ -15,12 +12,20 @@ public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long question_id;
+    private Long comment_id;
 
-    private String question;
+    private String comment;
 
-    public Comment(Long question_id, String question) {
-        this.question_id = question_id;
-        this.question = question;
+    private String music_url;
+
+    @OneToOne
+    @JoinColumn(name = "diary_id")
+    private Diary diary;
+
+    public Comment(Long comment_id, String comment, String music_url, Diary diary) {
+        this.comment_id = comment_id;
+        this.comment = comment;
+        this.music_url = music_url;
+        this.diary = diary;
     }
 }
