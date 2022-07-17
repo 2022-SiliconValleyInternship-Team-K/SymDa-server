be727b2a33b494a92d19d43a7fc708befbfdf679
cje1903 <cje1396@gmail.com>
2022-07-18 01:57:54 +0900

    [STYLE] add remark

--- src/main/java/team_k/symda/Repository/DiaryRepository.java
+++ src/main/java/team_k/symda/Repository/DiaryRepository.java
@@ -2,29 +2,27 @@ package team_k.symda.Repository;
 
 import team_k.symda.Entity.Diary;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface DiaryRepository {
     /*
-    * Diary 저장
-    * */
+     * Diary 저장 (C)
+     */
     Diary save(Diary diary);
 
     /*
-     * id로 Diary 찾기
-     * id가 null일 경우를 대비해 optional로 감싸서 반환
+     * diary_id로 Diary 찾기 (R)
      * */
     Optional<Diary> findById(Long id);
 
     /*
-     * 월별 일기 반환
+     * 월별 일기 반환 (R)
      * */
     List<Diary> findByMonth(String month);
 
     /*
-     * id로 Diary 삭제
-     * */    
+     * id로 Diary 삭제 (D)
+     * */
     void deleteById(Long id);
 }
