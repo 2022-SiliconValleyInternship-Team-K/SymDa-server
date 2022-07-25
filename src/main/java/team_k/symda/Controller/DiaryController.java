c738852310fc9bc4f229deeb4a04cd03a5207fcb
cje1903 <cje1396@gmail.com>
2022-07-26 08:51:00 +0900

    [FEAT] add apis
    - 일기 작성 api ( image를 S3에 업로드 + 나머지 데이터를 받는  diaryCreateDto)
    - 일기 삭제 api ( id와 date 값으로 삭제 )
    - 일기 조회 api ( id와 date 값으로 조회 ) - 조회할 때, diary entity의 user 객체는 제외하고 줌
    - 월별 조회 api ( 일기, 감정, 식물 조회)
    - 오늘의 질문 조회 api ( 클라이언트에서 questionId 1~5 중 하나를 랜덤으로 선택해서 주면, 해당 Id의 question 객체를 줌 )

--- src/main/java/team_k/symda/Controller/DiaryController.java
+++ src/main/java/team_k/symda/Controller/DiaryController.java
@@ -1,14 +1,20 @@
 package team_k.symda.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import team_k.symda.Constants.Emotion;
 import team_k.symda.Entity.Diary;
+import team_k.symda.Entity.Question;
 import team_k.symda.Service.DiaryService;
+import team_k.symda.Service.QuestionService;
 import team_k.symda.Service.UserRequest;
+import team_k.symda.domain.DiaryCreateRequestDto;
+import team_k.symda.domain.DiaryResponseDto;
+import team_k.symda.domain.MonthlyEmotionDiaryResponseDto;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
@@ -20,12 +26,14 @@ import java.util.Optional;
 @Controller
 public class DiaryController {
 
-    // 의존성 주입 (생성자가 하나인 경우 생략 가능)
+    // 의존성 주입 (DI)
     private final DiaryService diaryService;
+    private final QuestionService questionService;
 
     @Autowired
-    public DiaryController(DiaryService diaryService) {
+    public DiaryController(DiaryService diaryService, QuestionService questionService) {
         this.diaryService = diaryService;
+        this.questionService = questionService;
     }
 
     /*
@@ -33,17 +41,20 @@ public class DiaryController {
      * */
     @ResponseBody   // Long 타입을 리턴하고 싶은 경우 붙여야 함 (Long - 객체)
     @PostMapping(value="/diary/new",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public Long saveDiary(HttpServletRequest request, @RequestParam(value="image") MultipartFile image, Diary diary) throws IOException {
+    // form-data/multipart -> 파라미터 처리 형식으로 받기
+    public DiaryResponseDto saveDiary(@RequestParam(value="image") MultipartFile image, @ModelAttribute DiaryCreateRequestDto diaryCreateRequestDto) throws IOException {
+        // @ModelAttribute 생략 가능하지만,, 헷갈릴까봐 추가
         System.out.println("DiaryController.saveDiary");
         System.out.println(image);
-        System.out.println(diary);
+        System.out.println(diaryCreateRequestDto);
         System.out.println("------------------------------------------------------");
-        Long diaryId = diaryService.keepDiary(image, diary);
-        return diaryId;
+
+        DiaryResponseDto diaryResponseDto = diaryService.keepDiary(image, diaryCreateRequestDto);
+        return diaryResponseDto;
     }
 
     /*
-     * 일기 삭제 - DELETE
+     * id로 일기 삭제 - DELETE
      * */
     @ResponseBody
     @DeleteMapping("/diary/{diaryId}")
@@ -52,13 +63,32 @@ public class DiaryController {
     }
 
     /*
+     * date로 일기 삭제 - DELETE
+     * */
+    @ResponseBody
+    @DeleteMapping("/diary/date/{date}/delete")
+    public void deleteDiaryByDate(@PathVariable String date){
+        diaryService.deleteDiaryByDate(date);
+    }
+
+    /*
      * id로 일기 조회 - GET
      * */
     @ResponseBody
     @GetMapping("/diary/{diaryId}")
-    public Optional<Diary> getDiary(@PathVariable Long diaryId){
-        Optional<Diary> diary = diaryService.findDiary(diaryId);
+    public DiaryResponseDto getDiary(@PathVariable Long diaryId){
+        DiaryResponseDto diary = diaryService.findDiary(diaryId);
         return diary;
+    }
+
+    /*
+     * date 로 일기 조회 - GET
+     * */
+    @ResponseBody
+    @GetMapping("/diary/date/{date}")
+    public DiaryResponseDto getDiaryByDate(@PathVariable String date){
+        DiaryResponseDto diaryByDate = diaryService.findDiaryByDate(date);
+        return diaryByDate;
     }
 
     /*
@@ -66,8 +96,8 @@ public class DiaryController {
      * */
     @ResponseBody
     @GetMapping("/diary/monthly/{month}")
-    public List<Diary> getMonthlyDiary(@PathVariable String month){
-        List<Diary> monthlyDiary = diaryService.findMonthlyDiary(month);
+    public List<DiaryResponseDto> getMonthlyDiary(@PathVariable String month){
+        List<DiaryResponseDto> monthlyDiary = diaryService.findMonthlyDiary(month);
         return monthlyDiary;
     }
 
@@ -76,10 +106,11 @@ public class DiaryController {
      * */
     @ResponseBody
     @GetMapping("/diary/monthly/{month}/emotion")
-    public Map<Long, Emotion> getMonthlyEmotion(@PathVariable String month){
-        Map<Long, Emotion> monthlyEmotion = diaryService.findMonthlyEmotion(month);
+    public List<MonthlyEmotionDiaryResponseDto> getMonthlyEmotion(@PathVariable String month){
+        List<MonthlyEmotionDiaryResponseDto> monthlyEmotion = diaryService.findMonthlyEmotion(month);
         return monthlyEmotion;
     }
+
 
     /*
      * 식물 상태 조회 - GET
@@ -92,6 +123,16 @@ public class DiaryController {
     }
 
     /*
+     * 오늘의 질문 조회 - GET
+     * */
+    @ResponseBody
+    @GetMapping("/diary/new/question/{questionId}")
+    public Question getTodayQuestion(@PathVariable Long questionId){
+        Question question = questionService.findQuestion(questionId);
+        return question;
+    }
+
+    /*
      * 유저의 새 일기 내용 1개 조회 - GET
      * */
     @ResponseBody
@@ -100,7 +141,7 @@ public class DiaryController {
         HttpSession session = request.getSession();
         UserRequest user = (UserRequest) session.getAttribute("loginMember");
         String diary = diaryService.findTop1Diary(user.getUser_id());
-//        String diary = diaryService.findTop1Diary(user_id);
+//      String diary = diaryService.findTop1Diary(user_id);
         return diary;
     }
 
